a00f077951f85b6277dd92313af98952c995c62c
Oleksii-Goloborodko <49151514+Oleksii-Goloborodko@users.noreply.github.com>
2019-10-27 18:26:53 +0200

    Update laba.1.py

--- laba.1.py
+++ laba.1.py
@@ -1,13 +1,1 @@
-sourceFile = open('text.txt','a')
-a = int(input())
-b = int(input())
-c = int(input())
 
-m = a
-if m < b:
-    m = b
-if m < c:
-    m = c
-
-print(m, file = sourceFile)
-sourceFile.close()
