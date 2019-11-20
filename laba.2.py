4fb1ce46644f11b73314ae148c931e3489c8458d
Oleksii-Goloborodko <49151514+Oleksii-Goloborodko@users.noreply.github.com>
2019-11-20 16:48:56 +0200

    Update laba.2.py

--- laba.2.py
+++ laba.2.py
@@ -1,1 +1,16 @@
+import datetime
 
+year = int(input('Enter a year'))
+month = int(input('Enter a month'))
+day = int(input('Enter a day'))
+
+date1 = datetime.date(year, month, day)
+
+print ("the first date: " + str(date1))
+
+date2 = datetime.date.today()
+print(date2)
+if (date1 == date2):
+   print("Date Time 1 is equal to Date Time 2")
+else:
+   print("Date times are unequal")
